2490e1feb57bcceaea59c36e9046c2f0ce5b74e8
nguyenngochoang255 <nguyenngochoang255@gmail.com>
2026-07-07 10:29:15 +0700

    Add files via upload

---
+++ Debug/Core/Src/subdir.mk
@@ -0,0 +1,82 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/ARHS.c \
+../Core/Src/Filter.c \
+../Core/Src/ICM42688.c \
+../Core/Src/IMU_CALIBRATE.c \
+../Core/Src/MS5611.c \
+../Core/Src/PID_Controller.c \
+../Core/Src/dma.c \
+../Core/Src/gpio.c \
+../Core/Src/i2c.c \
+../Core/Src/main.c \
+../Core/Src/register_board_config.c \
+../Core/Src/register_comm_config.c \
+../Core/Src/spi.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f4xx.c \
+../Core/Src/tim.c \
+../Core/Src/usart.c 
+
+OBJS += \
+./Core/Src/ARHS.o \
+./Core/Src/Filter.o \
+./Core/Src/ICM42688.o \
+./Core/Src/IMU_CALIBRATE.o \
+./Core/Src/MS5611.o \
+./Core/Src/PID_Controller.o \
+./Core/Src/dma.o \
+./Core/Src/gpio.o \
+./Core/Src/i2c.o \
+./Core/Src/main.o \
+./Core/Src/register_board_config.o \
+./Core/Src/register_comm_config.o \
+./Core/Src/spi.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f4xx.o \
+./Core/Src/tim.o \
+./Core/Src/usart.o 
+
+C_DEPS += \
+./Core/Src/ARHS.d \
+./Core/Src/Filter.d \
+./Core/Src/ICM42688.d \
+./Core/Src/IMU_CALIBRATE.d \
+./Core/Src/MS5611.d \
+./Core/Src/PID_Controller.d \
+./Core/Src/dma.d \
+./Core/Src/gpio.d \
+./Core/Src/i2c.d \
+./Core/Src/main.d \
+./Core/Src/register_board_config.d \
+./Core/Src/register_comm_config.d \
+./Core/Src/spi.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f4xx.d \
+./Core/Src/tim.d \
+./Core/Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -DUSE_FULL_LL_DRIVER -DARM_MATH_CM4 -D__FPU_PRESENT=1 -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/nguye/STM32CubeIDE/FC_F407/FC_F07/Middlewares/ST/ARM/DSP/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+
+.PHONY: clean-Core-2f-Src
